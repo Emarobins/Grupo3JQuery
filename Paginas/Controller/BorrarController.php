ec7ea337eb9844fa02f509611e14e427bd70a9d3
emanuel <emanuel@gmail.com>
2024-10-15 16:02:18 -0500

    hola

---
+++ Paginas/Controller/BorrarController.php
@@ -0,0 +1,14 @@
+<?php
+    include "../Model/Repository/ProductoRepository.php";
+
+    $ProductoRepository = new ProductoRepository();
+
+    if (isset($_GET["cod_prod"])) {
+        $cod_prod = $_GET["cod_prod"];
+
+        $curdproducto->BorrarProducto($cod_prod);
+
+        header("Location: ../vista/lista_producto.php");
+        exit();
+    }
+?>
